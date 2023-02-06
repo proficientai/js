2596e9139e16f9d13e56a33687171f1cfb9a417b
Anar Kafkas <anarkafkas@gmail.com>
2023-02-06 12:11:48 +0300

    Implement getUserHmac() function

--- apps/example-client/pages/index.tsx
+++ apps/example-client/pages/index.tsx
@@ -1,10 +1,31 @@
 import { InteractionView } from '@proficient/react-sdk';
+import axios from 'axios';
+import { useCallback, useEffect } from 'react';
+
+import type { ResponseBody } from './api/hmac';
 
 const proficientApiKey = 'pk_USjnILHKBxXeJtiJ5WtAquoRb4IXjQTz1ScvRWTV6XduKxQ4at0YYxRpY28Ce0Qug6fkhm5R';
 const agentId = 'ag_T2SW501k1RGk8ZUv2Kaz11Kh';
 const userExternalId = 'user123';
 
+const axiosInstance = axios.create({
+  baseURL: '/api',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
 export default function ExampleClient() {
+  const getUserHmac = useCallback(async () => {
+    const { data: resBody } = await axiosInstance.post<ResponseBody>('/hmac', { userId: userExternalId });
+    if (!resBody.success) throw new Error(resBody.error.message);
+    return resBody.data.hmac;
+  }, []);
+
+  useEffect(() => {
+    getUserHmac().then((hmac) => console.log(`hmac:`, hmac));
+  }, [getUserHmac]);
+
   return (
     <div>
       <h1>Example Client</h1>
