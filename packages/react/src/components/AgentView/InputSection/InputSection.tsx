d6ebc6f73b3a5bdcbeed8923275c94068e7f5f81
Anar Kafkas <anarkafkas@gmail.com>
2023-03-23 23:35:24 +0300

    Improve colors in InputSection

--- packages/react/src/components/AgentView/InputSection/InputSection.tsx
+++ packages/react/src/components/AgentView/InputSection/InputSection.tsx
@@ -2,6 +2,7 @@
 import { css } from '@emotion/react';
 import TextareaAutosize from 'react-textarea-autosize';
 
+import { colors } from '../../../styles';
 import type { InputSectionProps } from './types';
 
 export function InputSection({ onClickSend, onInputChange, placeholder, textAreaRef }: InputSectionProps) {
@@ -10,28 +11,31 @@ export function InputSection({ onClickSend, onInputChange, placeholder, textArea
       css={css`
         display: flex;
         flex-direction: column;
-        border-top: 1px solid gray;
+        border-top: 1px solid ${colors.gray[700]};
         padding-left: 20px;
         padding-right: 20px;
         padding-top: 12px;
         padding-bottom: 12px;
+        background-color: ${colors.gray[900]};
       `}>
       <div
         css={css`
           display: flex;
           flex-direction: column;
           border: 1px solid gray;
-          border-radius: 6px;
+          border-radius: 12px;
           overflow: hidden;
+          border-color: ${colors.gray[700]};
 
           &:focus-within {
-            border-color: blue;
+            border-color: ${colors.gray[500]};
           }
         `}>
         <TextareaAutosize
           ref={textAreaRef}
           placeholder={placeholder}
           css={css`
+            background-color: transparent;
             resize: none;
             border: none;
             padding-top: 8px;
@@ -40,6 +44,7 @@ export function InputSection({ onClickSend, onInputChange, placeholder, textArea
             padding-right: 12px;
             outline: 2px solid transparent;
             outline-offset: 2px;
+            color: ${colors.gray[100]};
           `}
           minRows={4}
           onChange={(e) => {
