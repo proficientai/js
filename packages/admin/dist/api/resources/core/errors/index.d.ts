1cf2d7dc657ab048b357aee5ac57db09957989ee
Anar Kafkas <anarkafkas@gmail.com>
2023-04-14 16:55:25 +0300

    Init @proficient/admin package

---
+++ packages/admin/dist/api/resources/core/errors/index.d.ts
@@ -0,0 +1,7 @@
+export * from "./InvalidRequestError";
+export * from "./InvalidCredentialsError";
+export * from "./ForbiddenError";
+export * from "./ResourceNotFoundError";
+export * from "./ConflictError";
+export * from "./InternalError";
+export * from "./ServiceUnavailableError";
---
+++ packages/admin/dist/api/resources/messages/client/requests/index.d.ts
@@ -0,0 +1,1 @@
+export { ListMessagesRequest } from "./ListMessagesRequest";
