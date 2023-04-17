eeb1d104477e1b02f9d653dcac3ce9f2c556818f
Anar Kafkas <anarkafkas@gmail.com>
2023-04-18 01:31:10 +0300

    Delete @proficient/api

--- packages/api/src/index.ts
+++
@@ -1,8 +0,0 @@
-export * from './resources/Agent';
-export * from './resources/AgentConfig';
-export * from './resources/Interaction';
-export * from './resources/Message';
-export * from './helpers';
-export * from './types';
-export type { AdminApi } from './admin';
-export type { ClientApi } from './client';
