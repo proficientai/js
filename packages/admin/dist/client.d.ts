529ec021ce80eaa82f41ce79d60a4a0bfb75d506
Anar Kafkas <anarkafkas@gmail.com>
2023-04-23 15:44:53 +0300

    Commit changes

---
+++ packages/admin/dist/client.d.ts
@@ -0,0 +1,7 @@
+import { ProficientClient as _ProficientClient } from '../core';
+export interface ProficientClientConfig {
+    environment: string;
+    secretKey: string;
+}
+export declare function createProficientClient(config: ProficientClientConfig): _ProficientClient;
+//# sourceMappingURL=client.d.ts.map
---
+++ packages/admin/dist/index.d.ts
@@ -0,0 +1,3 @@
+export { createProficientClient, type ProficientClientConfig } from './client';
+export { type Proficient, ProficientEnvironment } from '../core';
+//# sourceMappingURL=index.d.ts.map
