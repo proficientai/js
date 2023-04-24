780e19d799915612437123927b174f3491150a63
Anar Kafkas <anarkafkas@gmail.com>
2023-04-24 16:54:10 +0300

    Don't commit dist for now

--- packages/admin/dist/client.d.ts
+++
@@ -1,7 +0,0 @@
-import { ProficientClient as _ProficientClient } from '../core';
-export interface ProficientClientConfig {
-    environment: string;
-    secretKey: string;
-}
-export declare function createProficientClient(config: ProficientClientConfig): _ProficientClient;
-//# sourceMappingURL=client.d.ts.map
--- packages/client/dist/Client.d.ts
+++
@@ -1,9 +0,0 @@
-import { ProficientClient as _ProficientClient } from '../core';
-export interface ProficientClientConfig {
-    environment: string;
-    apiKey: string;
-    userExternalId: string;
-    userHmac?: string;
-}
-export declare function createProficientClient(config: ProficientClientConfig): _ProficientClient;
-//# sourceMappingURL=client.d.ts.map
