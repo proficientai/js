8362b9109e23b9067240aab416617138c8e4aa77
Anar Kafkas <anarkafkas@gmail.com>
2023-06-11 11:24:46 +0300

    Remove `secretKey` from client api sdk options

--- packages/client/core/Client.d.ts
+++ packages/client/core/Client.d.ts
@@ -8,7 +8,6 @@ import { Messages } from "./api/resources/messages/client/Client";
 import { Users } from "./api/resources/users/client/Client";
 export declare namespace ProficientClient {
     interface Options {
-        secretKey?: core.Supplier<string | undefined>;
         apiKey?: core.Supplier<string | undefined>;
         userExternalId?: core.Supplier<string | undefined>;
         userHmac?: core.Supplier<string | undefined>;
